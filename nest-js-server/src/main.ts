2ff516fecde011ceca74fb6336adf81a09484c98
Robert <robert.haidl@stud.fh-campuswien.ac.at>
2023-11-27 18:44:22 +0100

    added routing

--- nest-js-server/src/main.ts
+++ nest-js-server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.use(express.static(path.join(__dirname, '../../dist')));
+
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
