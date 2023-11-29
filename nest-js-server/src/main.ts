c40f047fc0adfcc414c88e79108a2ed92e4038da
Robert <robert.haidl@stud.fh-campuswien.ac.at>
2023-11-29 20:40:00 +0100

    fixed routing

--- nest-js-server/src/main.ts
+++ nest-js-server/src/main.ts
@@ -7,9 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  // Serve static files from the "../src/dist" directory for your Angular app
   app.use(express.static(path.join(__dirname, '../../dist')));
 
-  app.use('*', (req, res) => {
+  // Redirect all other requests to your Angular app's entry point
+  app.use('index', (req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/index.html'));
   });
 
